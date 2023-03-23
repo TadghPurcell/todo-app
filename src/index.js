6f8c698a569ada2e95c8d18f0d6e3ae816ce6741
Tadgh Purcell <tadghp@gmail.com>
2023-03-23 10:13:31 +0000

    Initialize sass

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import MakeToDoItem from "./makeToDoItem";
+import MakeToDoItem from './makeToDoItem';
+import './style.scss';
+
+const AllToDoProjects = {};
 
 const testItem = new MakeToDoItem(
-  "Go to shop",
-  "go to the shop and buy food",
-  "today",
-  "Tomorrow",
-  "low"
+  'Go to shop',
+  'go to the shop and buy food',
+  'today',
+  'Tomorrow',
+  'low'
 );
 
 console.log(testItem);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,33 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
+  mode: 'production',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg|jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
   },
 };
